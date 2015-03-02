2fa4a8fb27d4e2a0b1654a05082c6d3acb291a50
Richard Washer <richard@rwasher.com>
2015-03-02 10:22:05 +0000

    add prefix path option

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ module.exports = function(out, options) {
     if (options.absolute) {
       path = file.path;
     } else {
-      path = file.path.replace(file.base, '');
+      options.prefix = options.prefix || '';
+      path = file.path.replace(file.base, options.prefix);
       //path = file.path.replace(new RegExp('^' + __dirname + '/'), '');
     }
-    console.log(path);
     filePaths.push(path);
   };
 
